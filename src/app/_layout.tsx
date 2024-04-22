b0ca2d2a5486373a4fc3e5919b392602d923e7a2
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-22 18:27:16 -0300

    chore: configured @expo-google-fonts & add loading if fonts were not loaded

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,27 @@
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '../components/loading'
 
 import '../styles/global.css'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
   return (
     <>
       <StatusBar style="light" />
-      <Slot />
+      {fontsLoaded ? <Slot /> : <Loading />}
     </>
   )
 }

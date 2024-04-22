eaeb8c37277ab172ae06e1b33629a5affaa621c6
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-22 18:10:27 -0300

    chore: configure tailwindcss/nativewind and babel transpiler/metro bundler & extended fonts/colors tokens default theme

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-2xl text-white">Pass.in</Text>
+    </View>
+  )
+}
